e898900f785277d9b3d185023e1603fed959a945
Romain7426 <romain7426@example.com>
2025-03-06 18:13:24 +0000

    e

--- src/global.h
+++ src/global.h
@@ -125,8 +125,8 @@
 #include <string.h> 
 //#include <tgmath.h> // includes the headers <math.h>and<complex.h> --- 
 #include <time.h> // clock & time --- 
-#include <wchar.h> 
-#include <wctype.h> 
+//#include <wchar.h> 
+//#include <wctype.h> 
 #include <ctype.h> // tolower toupper
 
 #include <locale.h>
